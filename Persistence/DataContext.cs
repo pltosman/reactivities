3869dee9ffd7f719d92dbd852a5f2d7b0c3bfa8d
Osman POLAT <polatosmann@gmail.com>
2020-11-24 00:16:07 +0300

    InitialCommit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Value>().HasData(
+            new Value {Id=1, Name ="Value 101"},
+            new Value {Id=2, Name ="Value 102"},
+            new Value {Id=3, Name ="Value 103"},
+            new Value {Id=4, Name ="Value 104"}
+
+
+
+            );
+        }
+
+        
+        public DbSet<Value> Values {get;set;}
+
+    }
+}
