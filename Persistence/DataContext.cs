6b6ae302e7aac344b112bf8d56c3f41cce0d7be2
Osman POLAT <polatosmann@gmail.com>
2020-11-26 03:03:20 +0300

    End of Section 4

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Domain;
+﻿using Domain;
 using Microsoft.EntityFrameworkCore;
 
 
@@ -28,6 +27,7 @@ namespace Persistence
 
         
         public DbSet<Value> Values {get;set;}
+        public DbSet<Activity> Activities {get;set;}
 
     }
 }
