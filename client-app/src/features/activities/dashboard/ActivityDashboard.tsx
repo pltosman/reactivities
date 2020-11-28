129a32a975d2b002df2750ef5b61fd4ed0930a70
Osman POLAT <polatosmann@gmail.com>
2020-11-28 21:19:57 +0300

    semantic ui fixed and activitylist dashboard added

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Grid, GridColumn } from 'semantic-ui-react'
+import { IActivity } from '../../../app/models/activity'
+import { ActivityDetails } from '../details/ActivityDetails'
+import { ActivityForm } from '../form/ActivityForm'
+import { ActivityList } from './ActivityList'
+
+
+interface IProps {
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
+}
+
+
+export const ActivityDashboard: React.FC<IProps> = ({ activities, selectActivity, selectedActivity }) => {
+    return (
+        <Grid>
+            <GridColumn width={10}>
+                <ActivityList activities={activities} selectActivity={selectActivity} />
+            </GridColumn>
+            <GridColumn width={6}>
+                {selectedActivity && <ActivityDetails activity={selectedActivity} />}
+                <ActivityForm />
+            </GridColumn>
+        </Grid>
+    )
+}
